8a490a8e43235e3460491c5764c14534bbe37050
Koji Takami <vzg03566@gmail.com>
2015-05-05 13:51:36 +0900

    add test program to check stream I/O.

---
+++ test/readjson.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+#include "../jsonXX.h"
+using namespace std;
+using namespace json;
+
+int main(int argc, char* argv[]) {
+    /***
+    if(argc < 0) {
+        
+    }
+    char* infilename = argv[1];
+    ifstream fin(infilename);
+    ***/
+    var json;
+    cin >> json;
+    cout << json << endl;
+}
